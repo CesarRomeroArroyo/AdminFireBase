ccc5b275bcbf298506a9edae44f3d3d7c548f272
Cesar Romero <cra1982@gmail.com>
2018-05-16 17:02:11 -0500

    add new features
    add new features and add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StoreModule } from '@ngrx/store';
+
 
 import { AppComponent } from './app.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { NavPerfilComponent } from './componentes/nav-perfil/nav-perfil.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent,
+    NavPerfilComponent
   ],
   imports: [
     BrowserModule
